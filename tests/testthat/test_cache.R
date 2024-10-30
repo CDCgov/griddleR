f0b86cd21a9650d9a41f949c87de2043cb96be20
Scott Olesen <ulp7@cdc.gov>
2024-10-30 12:50:30 -0400

    vector test

--- tests/testthat/test_cache.R
+++ tests/testthat/test_cache.R
@@ -21,3 +21,24 @@ test_that("cache works", {
     expect_setequal(output$x, c(1, 2))
   })
 })
+
+test_that("cache with vector", {
+  local({
+    tmpdir <- withr::local_tempdir()
+    path <- file.path(tmpdir, "results")
+
+    fun <- function(params) {
+      tibble::tibble(y = sum(params$x))
+    }
+
+    parameter_sets <- list(list(x = c(1, 2, 3)))
+
+    griddleR::run_cache(fun, parameter_sets, path)
+
+    output <- griddleR::query_cache(path) |>
+      dplyr::collect()
+
+    expect_equal(output$x, "num [1:3] 1 2 3")
+    expect_equal(output$y, 6)
+  })
+})
